53ab2f7a781ea9c5d57c05dd903278205d3e4420
henric.eriksson.ec <henric.eriksson@utb.ecutbildning.se>
2021-12-30 12:23:55 +0100

    Gallery (HTML, CSS and JS) added

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,15 @@
 /***************************************   HOME   ***************************************/
 
+function toggle(){
+   var toggle = document.getElementById("services-section");
+    if (toggle.style.display==="none") {
+        toggle.style.display = "block"
+    } else {
+        toggle.style.display = "none"
+    }
+}
 
+ 
 
 
 
